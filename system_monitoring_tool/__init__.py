5fe8a0c4ef0fa96be6604fd56869d3e96e09e6ef
Elias Eriksson <eliaseriksson95@gmail.com>
2019-09-05 23:37:36 +0200

    accidental commit

--- system_monitoring_tool/__init__.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from .manager import create_database
-
-
-PROJECT_ROOT = Path(__file__).absolute().parent.parent
-DATABASE_ADRESS = f'sqlite:////{str(PROJECT_ROOT.joinpath("db.db"))}'
-
-__all__ = ["system_monetoring_tool", "exceptions"]
